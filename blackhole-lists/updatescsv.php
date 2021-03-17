ae007322f749272af51eb594b7ad74c00b084cd1
Seth Rice <sethrice@gmail.com>
2021-03-16 23:25:07 -0600

    initial commit

---
+++ blackhole-lists/updatescsv.php
@@ -0,0 +1,8 @@
+"2017/10/26_13:48","photoscape.ch/Setup.exe","31.148.219.11","knigazdorovya.com.","trojan","-","14576"
+"2017/09/28_08:11","izeselet.hu/wp-content/uploads/2016/03/ch.js","87.229.63.171","s3.abplusz.hu.","coin mining","-","62292"
+"2017/06/02_08:38","sarahdaniella.com/swift/SWIFT%20$.pdf.ace","63.247.140.224","coriandertest.hmdnsgroup.com.","trojan","Registrar Abuse Contact abuse@gkg.net","19271"
+"2017/05/01_16:22","amazon-sicherheit.kunden-ueberpruefung.xyz","185.61.138.74","hosted-by.blazingfast.io.","phishing","-","49349"
+"2017/03/20_10:13","dieutribenhkhop.com/parking/pay/rd.php?id=10","84.200.4.125","125.0-255.4.200.84.in-addr.arpa.","Ransom, Fake.PCN, Malspam","-","31400"
+"2017/03/20_10:13","dieutribenhkhop.com/parking/","84.200.4.125","125.0-255.4.200.84.in-addr.arpa.","Ransom, Fake.PCN, Malspam","-","31400"
+"2017/03/20_10:13","fourthgate.org/Yryzvt","104.200.67.194","-","Ransom, Fake.PCN, Malspam","Charlie Dillon / godaddy@638united.com","8100"
+"2017/03/20_10:13","alegroup.info/ntnrrhst","194.87.217.87","mccfortwayne.org.","Ransom, Fake.PCN, Malspam","Lee Everton / lee_everton2002@yahoo.com","197695"
